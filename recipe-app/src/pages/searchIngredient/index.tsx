4ad1dfaca48e98caef195150e91e744b2b1bd926
Carlos Roberio <Carlosroberiojp@hotmail.com>
2023-10-06 12:36:15 -0300

    Warning fix

--- recipe-app/src/pages/searchIngredient/index.tsx
+++ recipe-app/src/pages/searchIngredient/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { CardsContainer, PageContent, Title, SearchBar } from "./styles";
 import axios from "axios";
