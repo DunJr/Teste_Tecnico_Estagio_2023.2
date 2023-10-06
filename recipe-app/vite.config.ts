2d5352e9694cf5afd33023bb5d65c543b07d8e11
Carlos Roberio <Carlosroberiojp@hotmail.com>
2023-10-06 13:27:12 -0300

    Deploy configuration

--- recipe-app/vite.config.ts
+++ recipe-app/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/Teste_Tecnico_Estagio_2023.2",
+});
